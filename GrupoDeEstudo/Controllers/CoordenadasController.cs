f07fbc4267d7cd7545c067b134203474ee209a53
Dayan Castro <dayanpuc@hotmail.com>
2018-11-06 23:02:43 -0300

    Alteracao do novo controller de coordenadas.

---
+++ GrupoDeEstudo/Controllers/CoordenadasController.cs
@@ -0,0 +1,20 @@
+﻿using GrupoDeEstudo.Models;
+using System.Web.Mvc;
+
+namespace GrupoDeEstudo.Controllers
+{
+    public class CoordenadasController : Controller
+    {
+        // GET: Coordenadas
+        public ActionResult Index()
+        {
+            CoordenadasCliente coord = new CoordenadasCliente()
+            {
+                Latitude = 11,
+                Longitude = 22,
+            };
+            
+            return View(coord);
+        }
+    }
+}
---
+++ GrupoDeEstudo/Models/CoordenadasCliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GrupoDeEstudo.Models
+{
+    public class CoordenadasCliente
+    {
+        public int Longitude { get; set; }
+
+        public int Latitude { get; set; }
+    }
+}
