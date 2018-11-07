94786f9e1f1ae38c592e2e686f5bb7a1d016203e
Dayan Castro <dayanpuc@hotmail.com>
2018-11-06 22:09:07 -0300

    Commit do projeto do Tanaka.

---
+++ GrupoDeEstudo/Controllers/ClienteController.cs
@@ -0,0 +1,69 @@
+﻿using GrupoDeEstudo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GrupoDeEstudo.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            IList<Cliente> clientes = new List<Cliente>();
+            var cliente = new Cliente();
+            cliente.Nome = "Daniel Yanagita";
+
+            List<Produto> produtos = new List<Produto>();
+
+            Produto p = new Produto();
+            p.Codigo = 1;
+            p.Descricao = "Computador";
+            produtos.Add(p);
+
+            p = new Produto();
+            p.Codigo = 2;
+            p.Descricao = "Mouse";
+            produtos.Add(p);
+
+            p = new Produto();
+            p.Codigo = 3;
+            p.Descricao = "Teclado";
+            produtos.Add(p);
+
+            cliente.Produtos = produtos;
+
+            clientes.Add(cliente);
+
+            cliente = new Cliente();
+            cliente.Nome = "Francisco";
+
+            produtos = new List<Produto>();
+
+            p = new Produto();
+            p.Codigo = 1;
+            p.Descricao = "Roteador";
+            produtos.Add(p);
+
+            p = new Produto();
+            p.Codigo = 2;
+            p.Descricao = "Cabo";
+            produtos.Add(p);
+
+            p = new Produto();
+            p.Codigo = 3;
+            p.Descricao = "RJ45";
+            produtos.Add(p);
+
+            cliente.Produtos = produtos;
+
+            clientes.Add(cliente);
+
+            return View(clientes);
+        }
+
+    
+    }
+}
---
+++ GrupoDeEstudo/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GrupoDeEstudo.Models
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+
+        public List<Produto> Produtos { get; set; }
+    }
+}
