2b9d6c9626ce823ab49e1cbf2ce0303d60d6b0df
Francisco Otávio de Queiroz <franciscooq@yahoo.com.br>
2018-11-19 23:03:48 -0200

    Commit Francisco

---
+++ GrupoDeEstudo/Controllers/DurvalController.cs
@@ -0,0 +1,29 @@
+﻿using GrupoDeEstudo.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace GrupoDeEstudo.Controllers
+{
+    public class DurvalController : Controller
+    {     
+      
+
+        // GET: Durval
+        public ActionResult Index()
+        {
+            DurvalInicial init = new DurvalInicial()
+            {
+                Nome = "Durval Ferreira Pinto Marques",
+                Cidade = "Itaúna",
+                Estado = "MG",
+                Telefone = int.Parse("99591849"),
+        };
+
+            return View(init);
+
+        }
+    }
+}
---
+++ GrupoDeEstudo/Models/DurvalInicial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+    namespace GrupoDeEstudo.Models
+    {
+        public class DurvalInicial
+
+
+        {
+        
+        public String Nome { get; set; }
+        public String Cidade { get; set; }
+        public String Estado { get; set; }
+        public int Telefone { get; set; }
+
+    
+    }
+
+}
+
+
