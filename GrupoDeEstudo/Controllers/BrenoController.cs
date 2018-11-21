e6941d0a0de87b6519e0eb4b30104001dc424e68
Breno Lorran <brenolorran@hotmail.com.br>
2018-11-20 22:44:17 -0200

    Publicação Breno

---
+++ GrupoDeEstudo/Controllers/BrenoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GrupoDeEstudo.Models;
+
+namespace GrupoDeEstudo.Controllers
+{
+    public class BrenoController : Controller
+    {
+        public Breno breno = new Breno();
+
+        // GET: Breno
+        public ActionResult Index()
+        {
+            breno.nome = "Breno Lorran Nicacio";
+            breno.email = "breno@hotmail.com";
+
+            //return View(breno.getbreno());
+            return View(breno);
+        }
+    }
+}
